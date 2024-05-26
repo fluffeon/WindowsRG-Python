2ca0d31973e412be84296c112c667912b4f973d6
flookymonster <codenameflooent@gmail.com>
2024-05-26 03:03:08 -0300

    Asi lo dejo por hoy

--- main.py
+++ main.py
@@ -65,6 +65,49 @@ clock = pygame.time.Clock()
 
 MenuInicioPresionado=False
 ManteniendoApretado=False
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pg.draw.rect(screen, self.color, self.rect, 2)
+
 while True: 
       
     for event in pygame.event.get(): 
@@ -99,7 +142,6 @@ while True:
         pygame.draw.rect(ventana,color_blanco,(0, 145, 354, 437))
         pygame.draw.rect(ventana,color_negro,(4, 145, 352, 437))
         pygame.draw.rect(ventana,BarraDeTareas,(2, 145, 352, 400))
-
 
         for a in range(145,545,40):
             if 2 <= mouse[0] <= 352 and 140 <= mouse[1] <= a+40: 
